420efae9de55aeab9e01565e39ea6a2d1345d471
BMasquio <brunomasquio@gmail.com>
2022-04-29 16:02:42 -0300

    Fix login form names

--- resources/views/profile/logout-other-browser-sessions-form.blade.php
+++ resources/views/profile/logout-other-browser-sessions-form.blade.php
@@ -13,7 +13,7 @@
         </x-jet-action-message>
 
         <div>
-            {{ __('If necessary, you may log out of all of your other browser sessions across all of your devices. Some of your recent sessions are listed below; however, this list may not be exhaustive. If you feel your account has been compromised, you should also update your password.') }}
+            {{ __('If necessary, you may log out of all of your other browser sessions across all of your devices. Some of your recent sessions are listed below; however, this list may not be exhaustive. If you feel your account has been compromised, you should contact support.') }}
         </div>
 
         @if (count($this->sessions) > 0)
