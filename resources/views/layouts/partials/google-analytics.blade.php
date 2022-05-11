0bf58c26c2473811b93f9ba0416c6d1482fc065f
BMasquio <brunomasquio@gmail.com>
2022-05-11 14:19:08 -0300

    Google Analytics

--- resources/views/layouts/partials/google-analytics.blade.php
+++ resources/views/layouts/partials/google-analytics.blade.php
@@ -1,9 +1,11 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-N195QWQ3RW"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+@if(app()->environment('production'))
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-N195QWQ3RW"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-    gtag('config', 'G-N195QWQ3RW');
-</script>
+        gtag('config', 'G-N195QWQ3RW');
+    </script>
+@endIf
