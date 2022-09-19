a49c3ed96ce465ce43b84ddada042f8ce09dd542
ricardocamargo07 <53793788+ricardocamargo07@users.noreply.github.com>
2022-09-19 17:30:28 -0300

    more tests

--- tests/Browser/Pages/SectorsTest.php
+++ tests/Browser/Pages/SectorsTest.php
@@ -10,7 +10,7 @@ use Tests\DuskTestCase;
 
 class SectorsTest extends DuskTestCase
 {
-     /*
+     /**
      * @test
      * @group tests_createSectors
      * @group link
@@ -54,6 +54,8 @@ class SectorsTest extends DuskTestCase
         $sector = Sector::all()->random(1)->toArray()[0];
 
         //Wrong Search
+        $generateSector =
+
         $this->browse(function ($browser) use ($user) {
             $browser
               ->loginAs($user->id)
@@ -108,5 +110,53 @@ class SectorsTest extends DuskTestCase
         $this->assertDatabaseHas('sectors', [
           'name' =>'**'.$randomSector->name.'**',
       ]);
+            ->visit('/sectors')
+            ->assertSee('Nome')
+            ->click('#novo')
+            ->assertPathIs('/sectors/create')
+            ->type('#name',$generateSector['name'])
+            ->assertChecked('@checkboxSectors')
+            ->script('document.querySelectorAll("#submitButton")[0].click();');
+          $browser
+            ->assertPathIs('/sectors')
+            ->assertSee('Setor criado com sucesso!');
+        });
+        $this->assertDatabaseHas('sectors', [
+          'name' => $generateSector['name']
+      ]);
+    }
+
+     /**
+     * @test
+     * @group testSearch
+     * @group link
+     */
+
+     // Dusk - Procura um setor
+     public function testSearch()
+    {
+        $user = User::factory()->create();
+        $sector = Sector::all()->random(1)->toArray()[0];
+
+        //Wrong Search
+        $this->browse(function ($browser) use ($user) {
+            $browser
+              ->loginAs($user->id)
+                ->visit('/sectors')
+                ->type('@search-input', '132312312vcxvdsf413543445654')
+                ->click('@search-button')
+                ->waitForText('Nenhum Setor encontrado',8)
+                ->assertSee('Nenhum Setor encontrado');
+        });
+
+        //Right Search
+        $this->browse(function ($browser) use ($user,$sector) {
+          $browser
+            ->loginAs($user->id)
+              ->visit('/sectors')
+              ->type('@search-input', $sector['name'])
+              ->click('@search-button')
+              ->assertSee($sector['id']);
+      });
     }
 }
