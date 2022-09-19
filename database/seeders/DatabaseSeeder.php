57af758c7d26fbec58f54a67e26ed87b7f740651
ricardocamargo07 <53793788+ricardocamargo07@users.noreply.github.com>
2022-09-19 14:56:17 -0300

    getting_started
    Testes da aplicação.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        
+
         $this->call(SectorsTableSeeder::class);
 
         $this->call(EventTypesTableSeeder::class);
