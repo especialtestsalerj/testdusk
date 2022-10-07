71401884c85536b8292b50592af862dcc60b3e02
ricardocamargo07 <53793788+ricardocamargo07@users.noreply.github.com>
2022-10-07 14:46:09 -0300

    Tests

--- tests/Browser/Pages/EventTypesTest.php
+++ tests/Browser/Pages/EventTypesTest.php
@@ -5,34 +5,36 @@ namespace Tests\Feature;
 use App\Models\User;
 use App\Models\EventType;
 use Illuminate\Support\Facades\DB;
+use App\Http\Livewire\EventTypes\Index;
 use App\Support\Constants;
+use Livewire\Livewire;
 use Tests\DuskTestCase;
 
 class EventTypesTest extends DuskTestCase
-{   
-     /*
+{
+     /**
      * @test
      * @group tests_createEventTypes
      * @group link
      */
 
-    //Dusk - Criação de um novo EventTypes
-    public function tests_createSectors()
+    //Dusk - Criação de um novo Tipo de Ocorrência
+    public function tests_createEventTypes()
     {
         $user = User::factory()->create();
         $user->assign(Constants::ROLE_ADMINISTRATOR);
         $user->allow('*');
         $user->save();
         //dd($user->roles()->get());
-        $generateEventType = ['name'=>strtoupper(faker()->unique()->name)];
-    
+        $generateEventType = ['name'=>strtoupper(faker()->unique()->company)];
+
         $this->browse(function ($browser) use ($user,$generateEventType) {
           $browser
             ->loginAs($user->id)
-            ->visit('/event_types')
+            ->visit('/event-types')
             ->assertSee('Nome')
             ->click('#novo')
-            ->assertPathIs('/event_types/create')
+            ->assertPathIs('/event-types/create')
             ->script('document.querySelectorAll("#submitButton")[0].click();');
           $browser
             ->assertSee('Nome: preencha o campo corretamente.')
@@ -40,10 +42,9 @@ class EventTypesTest extends DuskTestCase
             ->assertChecked('@checkboxEventTypes')
             ->script('document.querySelectorAll("#submitButton")[0].click();');
           $browser
-            ->assertPathIs('/event_types')
-            ->assertSee('Tipo de ocorrência adicionada com sucesso!');
+            ->assertPathIs('/event-types')
+            ->assertSee('Tipo de Ocorrência adicionado com sucesso!');
         });
-        $this->assertDatabaseHas('event_types', ['name' => $generateEventType['name']]);
     }
 
      /**
@@ -51,36 +52,24 @@ class EventTypesTest extends DuskTestCase
      * @group testSearchEventTypes
      * @group link
      */
-    
-     // Dusk - Procura um Tipo de ocorrência
+
+     // Livewire - Procura um Tipo de Ocorrência
      public function testSearchEventTypes()
-    { 
+    {
+      
       $user = User::factory()->create();
       $user->assign(Constants::ROLE_ADMINISTRATOR);
       $user->allow('*');
       $user->save();
       $event_type = EventType::all()->random(1)->toArray()[0];
 
-        //Wrong Search
-        $this->browse(function ($browser) use ($user) {
-            $browser
-              ->loginAs($user->id)
-                ->visit('/event_types')
-                ->type('@search-input', '132312312vcxvdsf413543445654')
-                ->click('@search-button')
-                ->waitForText('Nenhum Tipo de Ocorrência encontrado',8)
-                ->assertSee('Nenhum Tipo de Ocorrência encontrado');
-        });
-        
-        //Right Search
-        $this->browse(function ($browser) use ($user,$event_type) {
-          $browser
-            ->loginAs($user->id)
-              ->visit('/event_types')
-              ->type('@search-input',$event_type['name'])
-              ->click('@search-button')
-              ->assertSee($event_type['id']);
-      });
+      Livewire::test(Index::class)
+          ->assertSee('Tipos de Ocorrência')
+          ->set('searchString',$event_type['name'])
+          ->assertSet('searchString',$event_type['name'])
+          ->assertSee($event_type['id'])
+          ->set('searchString','6662223')
+          ->assertSee('Nenhum Tipo de Ocorrência encontrado.');
     }
 
     /*
@@ -89,35 +78,35 @@ class EventTypesTest extends DuskTestCase
      * @group link
      */
 
-    //Dusk - Edição de um novo Tipo de ocorrência
-    public function tests_editSectors()
+    //Dusk e Livewire - Edição de um novo Tipo de Ocorrência
+    public function tests_editEventTypes()
     {
       $user = User::factory()->create();
       $user->assign(Constants::ROLE_ADMINISTRATOR);
       $user->allow('*');
       $user->save();
-      $randomEventType = DB::table('event_types')
+        $randomEventType = DB::table('event_types')
         ->inRandomOrder()
         ->first();
-    
+
         $this->browse(function ($browser) use ($user,$randomEventType) {
           $browser
             ->loginAs($user->id)
-            ->visit('/event_types')
-            ->type('@search-input', $randomEventType->name)
-            ->click('@search-button')
-            ->assertSee($randomEventType->id)
-            ->press('@event_type-'.$randomEventType->id)
+            ->visit('event-types/'.$randomEventType->id)
             ->type('#name','**'.$randomEventType->name.'**')
             ->check('@checkboxEventTypes')
             ->assertChecked('@checkboxEventTypes')
             ->script('document.querySelectorAll("#submitButton")[0].click();');
           $browser
-            ->assertPathIs('/event_types')
-            ->assertSee('Tipo de ocorrência alterada com sucesso!');
+            ->assertPathIs('/event-types')
+            ->assertSee('Tipo de Ocorrência alterado com sucesso!');
         });
-        $this->assertDatabaseHas('event_types', [
-          'name' =>'**'.$randomEventType->name.'**',
-      ]);
+        $this->assertDatabaseHas('event_types', ['name' =>'**'.$randomEventType->name.'**']);
+
+        Livewire::test(Index::class)
+          ->assertSee('Tipos de Ocorrência')
+          ->set('searchString',$randomEventType->name)
+          ->assertSet('searchString',$randomEventType->name)
+          ->assertSee($randomEventType->id);
     }
 }
