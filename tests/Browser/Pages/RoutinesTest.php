fa9225cf717a324c2974284c135090a75431e955
ricardocamargo07 <53793788+ricardocamargo07@users.noreply.github.com>
2022-10-31 14:39:23 -0300

    CircleCi and test

--- tests/Browser/Pages/RoutinesTest.php
+++ tests/Browser/Pages/RoutinesTest.php
@@ -25,7 +25,7 @@ class RoutinesTest extends DuskTestCase
         $user->save();
         //dd($user->roles()->get());
         $generateRoutine = Routine::factory()->create()->toArray();
-
+        dump($generateRoutine['entranced_at'][8]);
         $this->browse(function ($browser) use ($user,$generateRoutine) {
           $browser
             ->loginAs($user->id)
@@ -33,15 +33,25 @@ class RoutinesTest extends DuskTestCase
             ->assertSee('Rotinas')
             ->click('#novo')
             ->assertPathIs('/routines/create')
+            ->screenshot('1')
             ->script('document.querySelectorAll("#submitButton")[0].click();');
           $browser
             ->assertSee('Turno: preencha o campo corretamente.')
             ->assertSee('Responsável (Assunção): preencha o campo corretamente.')
             ->assertSee('Carga: preencha o campo corretamente.')
+            ->visit('/routines/create')
+            ->assertPathIs('/routines/create')
+            ->pause(1000)
+            ->screenshot('1')
             ->select('#shift_id',$generateRoutine['shift_id'])
+            ->assertSelected('#shift_id',$generateRoutine['shift_id'])
+            ->type('#entranced_at',str_replace($generateRoutine['entranced_at']))
+            ->screenshot('2');
+            //->script('entranced_at.value="2022-10-20 08:09";');
+          $browser
             ->type('#checkpoint_obs', $generateRoutine['checkpoint_obs'])
-            ->select('#entranced_user_id', $generateRoutine['entranced_user_id'])
-            ->select('#exited_user_id', $generateRoutine['exited_user_id'])
+            ->select('#entranced_user_id', rand(2,9))
+            ->select('#exited_user_id', rand(2,9))
             ->script('document.querySelectorAll("#submitButton")[0].click();');
           $browser
             ->assertPathIs('/routines')
