5a6f5e14e4ceba12717d76f1272fc22841e05a63
joaopedrobs <jpedrobs13@gmail.com>
2023-06-13 17:12:53 -0300

    Login funcionando

--- tests/Browser/Pages/RoutinesTest.php
+++ tests/Browser/Pages/RoutinesTest.php
@@ -243,6 +243,252 @@ class RoutinesTest extends DuskTestCase
         });
 
     }
+//    public function testVisitors()
+//    {
+//        $user = User::factory()->create();
+//        $user->assign(Constants::ROLE_ADMINISTRATOR);
+//        $user->allow('*');
+//        $user->save();
+//        $routine = Routine::all()
+//            ->where('status', '=>', 'true')
+//            ->random(1)
+//            ->toArray()[0];
+//        $event_type = EventType::all()
+//            ->random(1)
+//            ->toArray()[0];
+//        $sector = Sector::all()
+//            ->random(1)
+//            ->toArray()[0];
+//        $duty_user = User::all()
+//            ->random(1)
+//            ->toArray()[0];
+//
+//        $this->browse(function ($browser) use ($user, $routine, $event_type, $sector, $duty_user) {
+//            $browser
+//                ->loginAs($user->id)
+//                ->visit('/routines')
+//                ->assertSee('Rotinas')
+//                ->press('@manageRoutine-' . $routine['id'])
+//                ->script('document.getElementById("newVisitor").click()');
+//            $browser
+//                ->assertPathIs('/routines'. '/' . $routine['id'] . '/visitors/create' )
+//
+//                ->press('#submitButton')
+//                ->assertSee('CPF (Visitante): preencha o campo corretamente.')
+//                ->assertSee('Nome (Visitante): preencha o campo corretamente.')
+//                ->assertSee('Plantonista: preencha o campo corretamente.')
+//                ->assertSee('Observações: preencha o campo corretamente.');
+//            $this->insertDate(1, 'exited_at');
+//            $browser
+//                ->select('#sector_id', $sector['id'])
+//                ->select('#duty_user_id', $duty_user['id'])
+//                ->type('#cpf', '12312312387')
+//                ->click('#btn_buscar')
+//                ->pause(1000)
+//                ->type('#description', str_random(15))
+//                ->type('#full_name', str_random(10))
+//                ->type('#origin', str_random(5))
+//                ->press('#submitButton')
+//                ->assertPathIs('/routines/' . $routine['id'])
+//                ->assertSee('Visitante adicionado/a com sucesso!');
+//        });
+//    }
+//
+//    /**
+//     * @test
+//     * @group testCreateEditStuffs
+//     * @group link
+//     */
+//
+//    // Dusk - Cria/Altera um materiais detro de Rotina.
+//
+//    public function testStuffs()
+//    {
+//        $user = User::factory()->create();
+//        $user->assign(Constants::ROLE_ADMINISTRATOR);
+//        $user->allow('*');
+//        $user->save();
+//        $routine = Routine::all()
+//            ->where('status', '=>', 'true')
+//            ->random(1)
+//            ->toArray()[0];
+//        $event_type = EventType::all()
+//            ->random(1)
+//            ->toArray()[0];
+//        $sector = Sector::all()
+//            ->random(1)
+//            ->toArray()[0];
+//        $duty_user = User::all()
+//            ->random(1)
+//            ->toArray()[0];
+//
+//        $this->browse(function ($browser) use ($user, $routine, $event_type, $sector, $duty_user) {
+//            $browser
+//                ->loginAs($user->id)
+//                ->visit('/routines')
+//                ->assertSee('Rotinas')
+//                ->press('@manageRoutine-' . $routine['id'])
+//                ->script('document.getElementById("newStuff").click()');
+//
+//            $browser
+//                ->assertPathIs('/routines'. '/' . $routine['id'] . '/stuffs/create')
+//                ->screenshot('stuffs')
+//                ->press('#submitButton')
+//                ->assertSee('Plantonista: preencha o campo corretamente.')
+//                ->assertSee('Observações: preencha o campo corretamente.');
+//            $this->insertDate(1, 'exited_at');
+//            $browser
+//                ->select('#sector_id', $sector['id'])
+//                ->select('#duty_user_id', $duty_user['id'])
+//                ->type('#description', str_random(5))
+//                ->press('#submitButton')
+//                ->assertPathIs('/routines/' . $routine['id'])
+//                ->assertSee('Material adicionado com sucesso!');
+//            // $stuff = DB::table('stuffs')
+//            //     ->where('routine_id', '=', $routine['id'])
+//            //     ->first();
+//            // $browser->visit('/stuffs/' . $stuff->id)->type('#description', str_random(15));
+//            // $this->insertDate(0, 'entranced_at');
+//            // $this->insertDate(1, 'exited_at');
+//            // $browser
+//            //     ->press('#submitButton')
+//            //     ->assertPathIs('/routines/' . $routine['id'])
+//            //     ->assertSee('Material alterado com sucesso!');
+//        });
+//    }
+//
+//    /**
+//     * @test
+//     * @group testCreateEditCautions
+//     * @group link
+//     */
+//
+//    // Dusk - Cria/Altera uma Cautela de Armas detro de Rotina.
+//
+//    public function testCautions()
+//    {
+//        $user = User::factory()->create();
+//        $user->assign(Constants::ROLE_ADMINISTRATOR);
+//        $user->allow('*');
+//        $user->save();
+//        $routine = Routine::all()
+//            ->where('status', '=>', 'true')
+//            ->random(1)
+//            ->toArray()[0];
+//        $event_type = EventType::all()
+//            ->random(1)
+//            ->toArray()[0];
+//        $sector = Sector::all()
+//            ->random(1)
+//            ->toArray()[0];
+//        $duty_user = User::all()
+//            ->random(1)
+//            ->toArray()[0];
+//        $visitor = Visitor::all()
+//            ->random(1)
+//            ->toArray()[0];
+//
+//        $this->browse(function ($browser) use (
+//            $user,
+//            $routine,
+//            $event_type,
+//            $sector,
+//            $duty_user,
+//            $visitor
+//        ) {
+//            $browser
+//                ->loginAs($user->id)
+//                ->visit('/routines')
+//                ->assertSee('Rotinas')
+//                ->press('@manageRoutine-' . $routine['id'])
+//                ->press('@newCaution')
+//                ->assertPathIs('/cautions/create/' . $routine['id'])
+//                ->press('#submitButton')
+//                ->assertSee('Visitante: preencha o campo corretamente.')
+//                ->assertSee('Tipo de Porte: preencha o campo corretamente.')
+//                ->assertSee('RG: preencha o campo corretamente.')
+//                ->assertSee('Núm. Certificado: preencha o campo corretamente.')
+//                ->assertSee('Validade Certificado: preencha o campo corretamente.')
+//                ->assertSee('Destino: preencha o campo corretamente.')
+//                ->assertSee('Plantonista: preencha o campo corretamente.')
+//                ->visit('/cautions/create/' . $routine['id']);
+//            $this->insertDate(0, 'started_at');
+//            $this->insertDate(1, 'concluded_at');
+//            $browser->pause(5000);
+//            $browser->script([
+//                'a = document.querySelector("[id=\'visitor_id\']");',
+//                'a.value=' . $visitor['id'] . ';',
+//                'a.dispatchEvent(new Event(\'input\'));',
+//                'a.dispatchEvent(new Event(\'change\'));',
+//            ]);
+//            $browser->pause(5000);
+//            $browser->script([
+//                'b = document.querySelector("[id=\'certificate_type\']");',
+//                'b.value=' . rand(1, 2) . ';',
+//                'b.dispatchEvent(new Event(\'input\'));',
+//                'b.dispatchEvent(new Event(\'change\'));',
+//            ]);
+//            $browser->type('#id_card', '441273312')->type('#certificate_number', '123123');
+//            $browser->script([
+//                'b = document.querySelector("[id=\'certificate_valid_until\']");',
+//                'b.value="2024-05-05";',
+//                'b.dispatchEvent(new Event(\'input\'));',
+//                'b.dispatchEvent(new Event(\'change\'));',
+//            ]);
+//            $browser
+//                ->select('#duty_user_id', $duty_user['id'])
+//                ->type('#description', str_random(5))
+//                ->press('#submitButton')
+//                ->assertPathIs('/routines/' . $routine['id'])
+//                ->assertSee('Cautela adicionada com sucesso!');
+//            $caution = DB::table('cautions')
+//                ->where('routine_id', '=', $routine['id'])
+//                ->first();
+//            $browser->visit('/cautions/' . $caution->id)->type('#description', str_random(15));
+//            $this->insertDate(0, 'started_at');
+//            $this->insertDate(1, 'concluded_at');
+//            $browser
+//                ->press('#submitButton')
+//                ->assertPathIs('/routines/' . $routine['id'])
+//                ->assertSee('Cautela alterada com sucesso!');
+//
+//            //adiciona uma arma
+//
+//            $browser
+//                ->visit('/cautions/' . $caution->id)
+//                ->press('@newWeapon')
+//                ->waitForText('Nova Arma')
+//                ->select('#weapon_type_id', rand(1, 3))
+//                ->pause(1000);
+//            $browser->script([
+//                'b = document.querySelector("[dusk=\'formDescription\']");',
+//                'b.value="artefato";',
+//                'b.dispatchEvent(new Event(\'input\'));',
+//                'b.dispatchEvent(new Event(\'change\'));',
+//            ]);
+//            $browser
+//                ->pause(1000)
+//                ->script([
+//                    'b = document.querySelector("[id=\'weapon_number\']");',
+//                    'b.value=' . rand(1999, 3000) . ';',
+//                    'b.dispatchEvent(new Event(\'input\'));',
+//                    'b.dispatchEvent(new Event(\'change\'));',
+//                ]);
+//            $browser
+//                ->pause(1000)
+//                ->select('#cabinet_id', rand(1, 2))
+//                ->select('#shelf_id', rand(2, 50))
+//                ->press('@submit')
+//                ->pause(2000)
+//                ->screenshot(6);
+//        });
+//    }
+//
+//    /**
+//     * @test
+//     * @group testFinishRoutine
+//     * @group link
+//     */
 
 
 }
