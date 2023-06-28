4daaab1bff23ff93f29618e58fbd2163ec17434e
marcoscoe <marcoscoe9@gmail.com>
2023-06-28 04:11:40 +0000

    button to top

--- resources/views/livewire/visitors/index.blade.php
+++ resources/views/livewire/visitors/index.blade.php
@@ -48,11 +48,6 @@
         </div>
     </div>
 
-    <div x-data="{ showButton: false }" @scroll.window="showButton = (window.pageYOffset > 100)" class="text-center pb-5 py-4"
-        style="position: fixed; bottom: 10px; right: 10px;" x-show="showButton">
-        <a href="#" @click.prevent="window.scrollTo({ top: 0, behavior: 'smooth' })" class="btn btn-primary"
-            title="Voltar para o topo">
-            <i class="fa-solid fa-arrow-up"></i>
-        </a>
-    </div>
+    @include('partials.button-to-top')
+    
 </div>
