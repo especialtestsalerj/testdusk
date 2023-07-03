7afbe78e316de44464efbd82aa4b7b77d1fea9c9
BMasquio <brunomasquio@gmail.com>
2023-07-03 14:37:07 -0300

    Fix migration

--- database/migrations/2023_06_23_121618_remove_old_id_from_visitors.php
+++ database/migrations/2023_06_23_121618_remove_old_id_from_visitors.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,8 +12,10 @@ return new class extends Migration
      */
     public function up()
     {
-         // Update rows with null old_id
-        $visitors = DB::table('visitors')->whereNull('old_id')->get();
+        // Update rows with null old_id
+        $visitors = DB::table('visitors')
+            ->whereNull('old_id')
+            ->get();
 
         foreach ($visitors as $visitor) {
             $latestUpdatedAt = DB::table('visitors')
@@ -27,15 +28,27 @@ return new class extends Migration
                 ->orWhere('id', $visitor->id)
                 ->min('created_at');
 
-            DB::table('visitors')->where('id', $visitor->id)->update([
-                'updated_at' => $latestUpdatedAt,
-                'created_at' => $earliestCreatedAt,
-            ]);
+            DB::table('visitors')
+                ->where('id', $visitor->id)
+                ->update([
+                    'updated_at' => $latestUpdatedAt,
+                    'created_at' => $earliestCreatedAt,
+                ]);
         }
 
-        // Delete rows with non-null old_id
-        DB::table('visitors')->whereNotNull('old_id')->delete();
+        DB::table('visitors')
+            ->whereNotNull('old_id')
+            ->get()
+            ->each(function ($row) {
+                DB::table('cautions')
+                    ->where('visitor_id', $row->id)
+                    ->update(['visitor_id' => $row->old_id]);
+            });
 
+        // Delete rows with non-null old_id
+        DB::table('visitors')
+            ->whereNotNull('old_id')
+            ->delete();
 
         // Remove the old_id column from the visitors table
         Schema::table('visitors', function (Blueprint $table) {
