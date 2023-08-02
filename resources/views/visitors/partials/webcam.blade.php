be8e94b900e6d6368649551a4e5e12c060729354
marcoscoe <marcoscoe9@gmail.com>
2023-08-02 15:35:02 +0000

    webcam

--- resources/views/visitors/partials/webcam.blade.php
+++ resources/views/visitors/partials/webcam.blade.php
@@ -1,9 +1,9 @@
  <!-- Button trigger modal -->
-<div class="pb-2 text-right">
+ <div class="pb-2 text-right">
      <button type="button" class="btn btn-primary pb-2" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
          Tirar foto
      </button>
-</div>
+ </div>
 
  <!-- Modal -->
  <div wire:ignore.self class="modal fade" data-bs-backdrop="static" data-bs-keyboard="false" id="staticBackdrop"
@@ -14,23 +14,28 @@
                  <h5 class="modal-title" id="staticBackdropLabel">Webcam</h5>
                  <button type="button" data-bs-dismiss="modal" class="btn-close" aria-label="Close"></button>
              </div>
-             <div class="modal-body">
-                 <div style="height: 100%">
 
-                     <button type="button" wire:click="takeSnapshot" onclick="take_snapshot()"
-                         style="border: none;
-                        padding: 0;
-                        background: none;"><i
-                             class="fa-solid fa-camera"></i></button>
-                     <label for="webcam_file" style="cursor:pointer;">
+             <div class="d-flex justify-content-evenly pt-3">
+                 <div>
+                     <label for="webcam_file" class="btn btn-outline-primary">
                          <i class="fas fa-upload"></i>
                      </label>
-                     <button wire:click.prevent="removeWebcamFile" type="button"
-                         style="border: none;
-        padding: 0;
-        background: none;"><i
-                             class="fas fa-eraser"></i></button>
+                 </div>
+                 <div class="{{ $webcam_file ? 'd-none' : ''}}">
+                     <button type="button" wire:click="takeSnapshot" onclick="take_snapshot()"
+                         class="btn btn-outline-primary">
+                         <i class="fa-solid fa-camera"></i>
+                     </button>
+                 </div>
+                 <div class="{{ $webcam_file ? '' : 'd-none' }}">
+                     <button wire:click.prevent="removeWebcamFile" type="button" class="btn btn-outline-primary">
+                         <i class="fas fa-eraser"></i>
+                     </button>
+                 </div>
+             </div>
 
+             <div class="modal-body {{ $webcam_file ? 'd-none' : '' }}">
+                 <div style="height: 100%">
                      <div class="row control-group" transition="expand" wire:ignore>
                          <div class="form-group col-12 floating-label-form-group controls">
                              <div class="card text-center">
@@ -70,58 +75,60 @@
 
              </div>
              @if ($webcam_file)
-                 <div class="row control-group" transition="expand" data-instance="{{ $iteration }}">
-                     <div class="form-group col-12 floating-label-form-group controls">
-                         <div class="card text-center">
-                             <div class="card-header">Recortar foto</div>
-                             <div class="card-body">
-                                 <div class="d-flex flex-column justify-content-center" wire:ignore
-                                     wire:key="force-rerender" x-data="{
-                                         setUp() {
-                                             const cropper = new Cropper(document.getElementById('preview_webcam_file'), {
-                                                 aspectRatio: 1 / 1,
-                                                 autoCropArea: 1,
-                                                 viewMode: 1,
-                                                 crop(event) {
-                                                     Livewire.emit('cropChanged', {
-                                                         x: event.detail.x,
-                                                         y: event.detail.y,
-                                                         width: event.detail.width,
-                                                         height: event.detail.height
-                                                     });
-                                                 }
-                                             })
-                                         }
-                                     }" x-init="setUp">
-                                     <div>
-                                         <img id="preview_webcam_file"
-                                             src="{{ $webcam_data_uri ? $webcam_file : $webcam_file->temporaryUrl() }}"
-                                             class="img-fluid" style="max-height: 300px">
+                 <div class="modal-body">
+                     <div class="row control-group" transition="expand" data-instance="{{ $iteration }}">
+                         <div class="form-group col-12 floating-label-form-group controls">
+                             <div class="card text-center">
+                                 <div class="card-header">Recortar foto</div>
+                                 <div class="card-body">
+                                     <div class="d-flex flex-column justify-content-center" wire:ignore
+                                         wire:key="force-rerender" x-data="{
+                                             setUp() {
+                                                 const cropper = new Cropper(document.getElementById('preview_webcam_file'), {
+                                                     aspectRatio: 1 / 1,
+                                                     autoCropArea: 1,
+                                                     viewMode: 1,
+                                                     crop(event) {
+                                                         Livewire.emit('cropChanged', {
+                                                             x: event.detail.x,
+                                                             y: event.detail.y,
+                                                             width: event.detail.width,
+                                                             height: event.detail.height
+                                                         });
+                                                     }
+                                                 })
+                                             }
+                                         }" x-init="setUp">
+                                         <div>
+                                             <img id="preview_webcam_file"
+                                                 src="{{ $webcam_data_uri ? $webcam_file : $webcam_file->temporaryUrl() }}"
+                                                 class="img-fluid" style="max-height: 300px">
+                                         </div>
                                      </div>
                                  </div>
                              </div>
                          </div>
                      </div>
-                 </div>
 
-                 <div class="row" x-init="window.canvas = document.getElementById('canvas');
-                 window.canvasBadge = document.getElementById('canvas-badge');
-                 window.base64Input = document.getElementById('photo');
-                 
-                 var ctx = canvas.getContext('2d');
-                 var ctxBadge = canvasBadge.getContext('2d');
-                 var image = new Image();
-                 image.src = '{{ $webcam_data_uri ? $webcam_file : $webcam_file->temporaryUrl() }}'; // Replace with your image source
-                 
-                 // When the image has loaded
-                 image.onload = function() {
-                     // Draw the image on the canvas, applying the crop
-                     ctx.drawImage(image, {{ $x ?? 0 }}, {{ $y ?? 0 }}, {{ $width ?? 400 }}, {{ $height ?? 400 }}, 0, 0, canvas.width, canvas.height);
-                     ctxBadge.drawImage(image, {{ $x ?? 0 }}, {{ $y ?? 0 }}, {{ $width ?? 400 }}, {{ $height ?? 400 }}, 0, 0, canvasBadge.width, canvasBadge.height);
-                     base64Input.value = canvas.toDataURL()
-                 };">
-                     {{--        <canvas wire:ignore id="canvas" width="400" height="400"></canvas> --}}
-                     <input wire:ignore id="photo" name="photo" type="hidden">
+                     <div class="row" x-init="window.canvas = document.getElementById('canvas');
+                     window.canvasBadge = document.getElementById('canvas-badge');
+                     window.base64Input = document.getElementById('photo');
+                     
+                     var ctx = canvas.getContext('2d');
+                     var ctxBadge = canvasBadge.getContext('2d');
+                     var image = new Image();
+                     image.src = '{{ $webcam_data_uri ? $webcam_file : $webcam_file->temporaryUrl() }}'; // Replace with your image source
+                     
+                     // When the image has loaded
+                     image.onload = function() {
+                         // Draw the image on the canvas, applying the crop
+                         ctx.drawImage(image, {{ $x ?? 0 }}, {{ $y ?? 0 }}, {{ $width ?? 400 }}, {{ $height ?? 400 }}, 0, 0, canvas.width, canvas.height);
+                         ctxBadge.drawImage(image, {{ $x ?? 0 }}, {{ $y ?? 0 }}, {{ $width ?? 400 }}, {{ $height ?? 400 }}, 0, 0, canvasBadge.width, canvasBadge.height);
+                         base64Input.value = canvas.toDataURL()
+                     };">
+                         {{--        <canvas wire:ignore id="canvas" width="400" height="400"></canvas> --}}
+                         <input wire:ignore id="photo" name="photo" type="hidden">
+                     </div>
                  </div>
              @endif
              <div class="modal-footer">
