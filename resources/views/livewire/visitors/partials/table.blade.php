04d5717de8d1295fa670111ee8671d7a5e7e8f7e
Orlando Vinicius <orlando.vinicius1987@gmail.com>
2023-08-23 11:48:44 -0300

    Alteração no layout

--- resources/views/livewire/visitors/partials/table.blade.php
+++ resources/views/livewire/visitors/partials/table.blade.php
@@ -136,7 +136,7 @@
                                     <div class="col-12 col-lg-10">
                                         <div class="row d-flex align-items-center">
                                             <div class="col-4 col-lg-1 text-center text-lg-start">
-                                                <img class="w-75" src="/img/no-photo.svg">
+                                                <img class="w-75" src="{{ $visitor->photo }}">
                                             </div>
                                             <div class="col-8 col-lg-11">
                                                 <div class="row d-flex align-items-center">
