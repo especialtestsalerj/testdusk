288fe574294137900767f0825dae78ab56a1bf2d
Orlando Vinicius <orlando.vinicius1987@gmail.com>
2023-08-24 14:44:44 -0300

    correção do merge

---
+++ resources/views/partials/dashboard-button-swipper.blade.php
@@ -0,0 +1,15 @@
+<div class="col-6 col-lg-3">
+    @can($permission)
+    <a href="{{$url}}">
+    @endcan
+        <span @can($permission)class="fa-stack fa-3x dashboard-active-icons"@else class="fa-stack fa-3x dashboard-inactive-icons"@endcan @can($permission) @if(isset($count)) data-count="{{$count}}" @endIf @endcan>
+            <i class="fa fa-circle fa-stack-2x"></i>
+            <i class="fa {{$ico}} fa-stack-1x fa-inverse"></i>
+        </span>
+        <h5 class="mt-2">
+            {{$title}}
+        </h5>
+    @can($permission)
+    </a>
+    @endcan
+</div>
