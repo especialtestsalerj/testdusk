c20aa93559dd40facd09287b29e21e529b69d717
Breno Trengrouse <brenot@gmail.com>
2023-08-31 13:14:42 -0300

    cautions header

--- resources/views/livewire/cautions/index.blade.php
+++ resources/views/livewire/cautions/index.blade.php
@@ -1,44 +1,44 @@
 
 <div>
     <div class="py-4 px-4">
-        <div class="">
-            <div class="row">
-                <div class="col-md-8">
-                    <h3 class="mb-0">Cautelas de Armas</h3>
-                    Rotina {{ $routine?->code }}
-                    @if ($routine?->status)
-                        <label class="badge bg-success"> ABERTA </label>
-                    @else
-                        <label class="badge bg-danger"> FINALIZADA </label>
-                    @endif
-                </div>
 
-                <div class="col-md-4">
-                    @if ($routine?->status)
-                        @include(
-                            'livewire.partials.search-form',
-                            [
-                                'btnNovoLabel' => 'Nova',
-                                'btnNovoTitle' => 'Nova Cautela de Arma',
-                                'routeSearch' => 'cautions.index',
-                                'routeSearchParams' => ['routine_id' => $routine_id],
-                                'routeCreate' => 'cautions.create',
-                                'routeCreateParams' => ['routine_id' => $routine_id, 'redirect' => 'cautions.index'],
-                            ]
-                        )
-                    @else
-                        @include(
-                            'livewire.partials.search-form',
-                            [
-                                'btnNovoLabel' => 'Nova',
-                                'btnNovoTitle' => 'Nova Cautela de Arma',
-                                'routeSearch' => 'cautions.index',
-                                'routeSearchParams' => ['routine_id' => $routine_id, 'redirect' => 'cautions.index'],
-                            ]
-                        )
-                    @endif
-                </div>
+        <div class="row mb-lg-3">
+            <div class="col-lg-6">
+                <h3 class="mb-0">Cautelas de Armas</h3>
+                Rotina {{ $routine?->code }}
+                @if ($routine?->status)
+                    <label class="badge bg-success"> ABERTA </label>
+                @else
+                    <label class="badge bg-danger"> FINALIZADA </label>
+                @endif
             </div>
+
+            <div class="col-lg-6">
+                @if ($routine?->status)
+                    @include(
+                        'livewire.partials.search-form',
+                        [
+                            'btnNovoLabel' => 'Nova',
+                            'btnNovoTitle' => 'Nova Cautela de Arma',
+                            'routeSearch' => 'cautions.index',
+                            'routeSearchParams' => ['routine_id' => $routine_id],
+                            'routeCreate' => 'cautions.create',
+                            'routeCreateParams' => ['routine_id' => $routine_id, 'redirect' => 'cautions.index'],
+                        ]
+                    )
+                @else
+                    @include(
+                        'livewire.partials.search-form',
+                        [
+                            'btnNovoLabel' => 'Nova',
+                            'btnNovoTitle' => 'Nova Cautela de Arma',
+                            'routeSearch' => 'cautions.index',
+                            'routeSearchParams' => ['routine_id' => $routine_id, 'redirect' => 'cautions.index'],
+                        ]
+                    )
+                @endif
+            </div>
+
         </div>
 
         <div class="">
--- resources/views/livewire/partials/search-form.blade.php
+++ resources/views/livewire/partials/search-form.blade.php
@@ -1,8 +1,8 @@
 @csrf
 
-<div class="form-group float-end">
-    <div class="d-flex row justify-content-end">
-        <div class="col-8 col-md-8">
+<div class="form-group">
+    <div class="d-flex row justify-content-end mt-3 mt-lg-0">
+        <div class="col-9 col-md-8 col-lg-10">
             <div class="input-group">
                 <input type="text" name="search" class="form-control" placeholder="Pesquisar..." wire:model.debounce.500ms="searchString" value="">
                 <span class="input-group-text">
@@ -18,7 +18,7 @@
             </div>
         </div>
         @if (isset($routeCreate))
-            <div class="col-4 col-md-4">
+            <div class="col-3 col-md-4 col-lg-2">
                 <a id="novo" href="{{ route($routeCreate, $routeCreateParams ?? []) }}" class="btn btn-primary text-white float-end" title="{{ $btnNovoTitle }}">
                     <i class="fa fa-plus"></i> {{ $btnNovoLabel }}
                 </a>
