3339b44a4ada5cb0aef925f5c7ba394f1dc0db6d
marcoscoe <marcoscoe9@gmail.com>
2023-08-31 18:10:03 +0000

    fix issue #212

---
+++ resources/views/livewire/cautions/partials/receipt.blade.php
@@ -0,0 +1,393 @@
+@if ($forPrinter)
+    <style type="text/css" media="screen">
+        .conteudo {
+            display: block !important;
+        }
+
+        #badge {
+            display: none;
+        }
+    </style>
+    <style type="text/css" media="print">
+        @page {
+            size: A4;
+            /* Define o tamanho da página como A4 */
+            margin: 0;
+            /* Remove as margens padrão da impressora */
+        }
+
+        /* reset.css */
+        html,
+        #badge,
+        div,
+        span,
+        applet,
+        object,
+        iframe,
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        p,
+        blockquote,
+        pre,
+        a,
+        abbr,
+        acronym,
+        address,
+        big,
+        cite,
+        code,
+        del,
+        dfn,
+        em,
+        img,
+        ins,
+        kbd,
+        q,
+        s,
+        samp,
+        small,
+        strike,
+        strong,
+        sub,
+        sup,
+        tt,
+        var,
+        b,
+        u,
+        i,
+        center,
+        dl,
+        dt,
+        dd,
+        ol,
+        ul,
+        li,
+        fieldset,
+        form,
+        label,
+        legend,
+        table,
+        caption,
+        tbody,
+        tfoot,
+        thead,
+        tr,
+        th,
+        td,
+        article,
+        aside,
+        canvas,
+        details,
+        embed,
+        figure,
+        figcaption,
+        footer,
+        header,
+        hgroup,
+        menu,
+        nav,
+        output,
+        ruby,
+        section,
+        summary,
+        time,
+        mark,
+        audio,
+        video {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            font-size: 100%;
+            font: inherit;
+            vertical-align: baseline;
+        }
+
+        /* HTML5 display-role reset for older browsers */
+        article,
+        aside,
+        details,
+        figcaption,
+        figure,
+        footer,
+        header,
+        hgroup,
+        menu,
+        nav,
+        section {
+            display: block;
+        }
+
+        .bg-danger,
+        .navbar,
+        .conteudo,
+        .conteudo-rodape {
+            display: none !important;
+        }
+
+        #badge {
+            line-height: 1;
+        }
+
+        ol,
+        ul {
+            list-style: none;
+        }
+
+        blockquote,
+        q {
+            quotes: none;
+        }
+
+        blockquote:before,
+        blockquote:after,
+        q:before,
+        q:after {
+            content: '';
+            content: none;
+        }
+
+        table {
+            border-collapse: collapse;
+            border-spacing: 0;
+        }
+
+        /* reset.css */
+
+        #badge {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 12px;
+            color: #333;
+        }
+
+        img {
+            width: 100px;
+            height: 100px;
+        }
+
+        p {
+            padding-top: 10px;
+        }
+
+        ul {
+            list-style: none;
+        }
+
+        ul li {
+            padding-bottom: 5px;
+        }
+
+        .left {
+            text-align: left;
+        }
+
+        .center {
+            text-align: center;
+        }
+
+        .right {
+            text-align: right;
+        }
+
+        .bold {
+            font-weight: bold;
+        }
+
+        table.table {
+            width: 100%;
+        }
+
+        table.table tr td {
+            padding: 5px;
+        }
+
+        .table .header td {
+            vertical-align: top;
+            padding-bottom: 0;
+        }
+
+        .table .header td p:first-child {
+            padding-top: 15px;
+        }
+
+        .table .header td p {
+            line-height: 10px;
+        }
+
+        .table .header .logo {
+            width: 80px;
+        }
+
+        .table .header .logo img {
+            width: 80px;
+            height: 80px;
+        }
+
+        .table .header td.left,
+        .table .footer td.left {
+            padding-left: 25px;
+        }
+
+        .table .header td.right,
+        .table .footer td.right {
+            padding-right: 25px;
+        }
+
+        .table .subject {
+            font-size: 110%;
+            background-color: #CCC;
+        }
+
+        .table .subject td {
+            padding: 5px 0;
+        }
+
+        .table .content td table {
+            margin: 5px 20px;
+        }
+
+        .table .content td {
+            padding: 2px;
+            position: relative;
+        }
+
+        .table .signature td {
+            font-size: 95%;
+            padding-top: 0;
+            padding-bottom: 5px;
+        }
+
+        .table .signature td p {
+            padding-top: 0;
+            padding-bottom: 2px;
+        }
+
+        .table .footer {
+            font-size: 90%;
+            border-top: 1px solid #DDD;
+            border-bottom: 1px dashed #333;
+        }
+
+        .table .footer td {
+            padding-bottom: 10px;
+        }
+
+        .table .footer p {
+            padding-top: 0;
+            font-style: italic;
+        }
+
+        @if (config('app.draft_document'))
+            .watermark {
+                position: absolute;
+                display: inline-block;
+                width: 100%;
+                top: 0;
+                left: 0;
+                opacity: 0.1;
+                text-align: center;
+                font-size: 120px;
+            }
+        @endif
+    </style>
+
+    <div id="badge"
+        @if ($forPrinter) x-init="
+            window.debounce = function (func, timeout = 1000){
+              let timer;
+              return (...args) => {
+                clearTimeout(timer);
+                timer = setTimeout(() => { func.apply(this, args); }, timeout);
+              };
+            }
+
+            const update = window.debounce(() => window.print());
+
+            document.addEventListener('printCautionReceipt', update)
+            " @endif>
+        @for ($i = 0; $i < 2; $i++)
+            <table class="table">
+                <tr class="header">
+                    <td class="left logo">
+                        <img alt="Logo Alerj" src="data:image/png;base64,{{ $logoBlob ?? '' }}" />
+                    </td>
+                    <td class="bold">
+                        <p>{{ mb_strtoupper(env('APP_COMPANY', 'Laravel')) }}</p>
+                        <p>DEPARTAMENTO DE SEGURANÇA</p>
+                        <p>ÓRGÃO DA PRESIDÊNCIA</p>
+                    </td>
+                    <td class="right">
+                        <p>{{ $caution?->started_at?->format('d/m/Y') ?? '-' }}</p>
+                        <p>{{ $caution?->started_at?->format('H:i') ?? '-' }}</p>
+                    </td>
+                </tr>
+                <tr class="subject">
+                    <td class="center bold" colspan="3">CAUTELA DE ARMA Nº
+                        {{ $caution?->protocol_number_formatted ?? '' }}</td>
+                </tr>
+                <tr class="content">
+                    <td colspan="3">
+                        <table>
+                            <tr>
+                                <td class="bold">NOME: </td>
+                                <td>{{ $caution->visitor->person->name ?? '' }} -
+                                    {{ $caution->visitor->document->type ?? '' }}
+                                    {{ $caution->visitor->document->number ?? '' }}</td>
+                            </tr>
+                            <tr>
+                                <td class="bold">DESTINO: </td>
+                                <td>{{ $caution->visitor?->sector?->name ?? '' }}</td>
+                            </tr>
+                            <tr>
+                                <td class="bold">ARMA(S): </td>
+                                <td>
+                                    <?php
+                                    $j = 0;
+                                    ?>
+                                    <ul>
+                                        @if ($cautionWeapons)
+                                            @foreach ($cautionWeapons as $cautionWeapon)
+                                                <?php
+                                                $j++;
+                                                ?>
+                                                <li>
+                                                    {{ str_pad($j, 2, '0', STR_PAD_LEFT) }})
+                                                    {{ $cautionWeapon->weaponType->name }}
+                                                    {{ $cautionWeapon->weapon_description }}
+                                                    {{ strlen($cautionWeapon->weapon_number) > 0 ? ' NÚM. ' . $cautionWeapon->weapon_number : '' }}
+                                                    {{ strlen($cautionWeapon->register_number) > 0 ? ' (Nº REG. SINARM ' . $cautionWeapon->register_number . ')' : '' }}
+                                                    {{ ' ' . $cautionWeapon->cabinet->name }}
+                                                    {{ '/ BOX ' . $cautionWeapon->shelf->name }}
+                                                </li>
+                                            @endforeach
+                                        @endif
+                                    </ul>
+                                </td>
+                            </tr>
+                        </table>
+                        @if (config('app.draft_document'))
+                            <div class="watermark">RASCUNHO</div>
+                        @endif
+                    </td>
+                </tr>
+                <tr class="content signature">
+                    <td class="center" colspan="3">
+                        <p>________________________________________________</p>
+                        <p>ASSINATURA DO/A RESPONSÁVEL PELA(S) ARMA(S)</p>
+                    </td>
+                </tr>
+                <tr class="footer">
+                    <td class="left">
+                        <p>© {{ mb_strtoupper(env('APP_OWNER', 'Laravel')) }}</p>
+                    </td>
+                    <td class="right" colspan="2">
+                        <p>{{ mb_strtoupper(env('APP_NAME', 'Laravel')) }}</p>
+                    </td>
+                </tr>
+            </table>
+        @endfor
+    </div>
+@endif
--- resources/views/livewire/cautions/partials/table.blade.php
+++ resources/views/livewire/cautions/partials/table.blade.php
@@ -1,4 +1,3 @@
-
 <div class="row mb-2">
     <div class="col-md-12">
         @forelse ($cautions as $caution)
@@ -10,10 +9,16 @@
                                 <span class="fw-bold">Protocolo:</span> {{ $caution?->protocol_number_formatted ?? '-' }}
                             </div>
                             <div class="col-12 col-lg-3 text-center text-lg-start">
-                                <span class="fw-bold">Abertura:</span> {{ $caution?->started_at?->format('d/m/Y \À\S H:i') ?? '-'}}
+                                <span class="fw-bold">Abertura:</span>
+                                {{ $caution?->started_at?->format('d/m/Y \À\S H:i') ?? '-' }}
                             </div>
                             <div class="col-12 col-lg-3 text-center text-lg-start">
-                                <span class="fw-bold">Fechamento:</span> @if(isset($caution?->concluded_at)) {{ $caution?->concluded_at?->format('d/m/Y \À\S H:i') }} @else <span class="badge bg-warning text-black">PENDENTE </span> @endif
+                                <span class="fw-bold">Fechamento:</span>
+                                @if (isset($caution?->concluded_at))
+                                    {{ $caution?->concluded_at?->format('d/m/Y \À\S H:i') }}
+                                @else
+                                    <span class="badge bg-warning text-black">PENDENTE </span>
+                                @endif
                             </div>
                             <div class="col-12 col-lg-4 text-center text-lg-start">
                                 <span class="fw-bold">Solicitante:</span> {{ $caution->visitor->person->name }}
@@ -25,18 +30,26 @@
                                 <span class="fw-bold">Plantonista:</span> {{ $caution->dutyUser->name }}
                             </div>
                             <div class="col-12 col-lg-4 text-center text-lg-end">
-                                @if(!$caution->hasWeapons())
-                                    <span class="badge bg-danger text-white"><i class="fa fa-exclamation-triangle"></i> SEM ARMA(S) </span>
+                                @if (!$caution->hasWeapons())
+                                    <span class="badge bg-danger text-white"><i class="fa fa-exclamation-triangle"></i>
+                                        SEM ARMA(S) </span>
                                 @endif
-                                @if($caution->hasPending())
-                                    <span class="badge bg-warning text-black"><i class="fa fa-exclamation-triangle"></i> ROTINA ANTERIOR </span>
+                                @if ($caution->hasPending())
+                                    <span class="badge bg-warning text-black"><i class="fa fa-exclamation-triangle"></i>
+                                        ROTINA ANTERIOR </span>
                                 @endif
-                                <a href="{{ route('cautions.receipt', ['routine_id' => $routine->id, 'id' => $caution->id, 'redirect' => $redirect]) }}" class="btn btn-link" title="Gerar comprovante"><i class="fa fa-lg fa-print"></i></a>
-                                <a href="{{ route('cautions.show', ['routine_id' => $routine_id, 'caution' => $caution->id, 'redirect' => $redirect, 'disabled' => true]) }}" class="btn btn-link" title="Detalhar"><i class="fa fa-lg fa-search"></i></a>
-                                @if($routine->status)
-                                    <a href="{{ route('cautions.show', ['routine_id' => $routine_id, 'caution' => $caution->id, 'redirect' => $redirect]) }}" class="btn btn-link" title="Alterar"><i class="fa fa-lg fa-pencil"></i></a>
-                                    @if(!$caution->hasPending())
-                                        <button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#caution-delete-modal{{ $caution->id }}" title="Remover">
+                                <span class="btn btn-link px-0 pt-0 pb-1"
+                                    wire:click="generateCautionReceipt({{ $caution->id }})" title="Imprimir Etiqueta">
+                                    <i class="fa fa-lg fa-print"></i>
+                                </span>
+                                <a href="{{ route('cautions.show', ['routine_id' => $routine_id, 'caution' => $caution->id, 'redirect' => $redirect, 'disabled' => true]) }}"
+                                    class="btn btn-link" title="Detalhar"><i class="fa fa-lg fa-search"></i></a>
+                                @if ($routine->status)
+                                    <a href="{{ route('cautions.show', ['routine_id' => $routine_id, 'caution' => $caution->id, 'redirect' => $redirect]) }}"
+                                        class="btn btn-link" title="Alterar"><i class="fa fa-lg fa-pencil"></i></a>
+                                    @if (!$caution->hasPending())
+                                        <button type="button" class="btn btn-link" data-bs-toggle="modal"
+                                            data-bs-target="#caution-delete-modal{{ $caution->id }}" title="Remover">
                                             <i class="fa fa-lg fa-trash"></i>
                                         </button>
                                     @endif
@@ -47,43 +60,58 @@
                 </div>
             </div>
             <!-- Modal -->
-            <div class="modal fade" id="caution-delete-modal{{ $caution->id }}" tabindex="-1" aria-labelledby="deleteModalLabelCaution" aria-hidden="true">
+            <div class="modal fade" id="caution-delete-modal{{ $caution->id }}" tabindex="-1"
+                aria-labelledby="deleteModalLabelCaution" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="deleteModalLabelCaution"><i class="fas fa-trash"></i> Remoção de Cautela</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <h5 class="modal-title" id="deleteModalLabelCaution"><i class="fas fa-trash"></i> Remoção de
+                                Cautela</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <form class="form" action="{{ route('cautions.destroy', ['routine_id' => $routine_id, 'id' => $caution->id]) }}" method="post">
+                            <form class="form"
+                                action="{{ route('cautions.destroy', ['routine_id' => $routine_id, 'id' => $caution->id]) }}"
+                                method="post">
                                 @csrf
                                 <input type="hidden" name="redirect" value="{{ $redirect }}">
                                 <div class="form-group">
                                     <label for="protocol_number">Protocolo</label>
-                                    <input type="text" class="form-control text-uppercase" name="protocol_number" id="protocol_number" value="{{ $caution?->protocol_number_formatted }}" disabled/>
+                                    <input type="text" class="form-control text-uppercase" name="protocol_number"
+                                        id="protocol_number" value="{{ $caution?->protocol_number_formatted }}"
+                                        disabled />
                                 </div>
                                 <div class="form-group">
                                     <label for="started_at">Data da Abertura</label>
-                                    <input type="datetime-local" max="3000-01-01T23:59" class="form-control text-uppercase" name="started_at" id="started_at" value="{{ $caution->started_at }}" disabled/>
+                                    <input type="datetime-local" max="3000-01-01T23:59"
+                                        class="form-control text-uppercase" name="started_at" id="started_at"
+                                        value="{{ $caution->started_at }}" disabled />
                                 </div>
                                 <div class="form-group">
                                     <label for="concluded_at">Data da Fechamento</label>
-                                    <input type="datetime-local" max="3000-01-01T23:59" class="form-control text-uppercase" name="concluded_at" id="concluded_at" value="{{ $caution->concluded_at }}" disabled/>
+                                    <input type="datetime-local" max="3000-01-01T23:59"
+                                        class="form-control text-uppercase" name="concluded_at" id="concluded_at"
+                                        value="{{ $caution->concluded_at }}" disabled />
                                 </div>
                                 <div class="form-group">
                                     <label for="visitor">Visitante</label>
-                                    <input type="text" class="form-control text-uppercase" name="visitor" id="visitor" value="{{ $caution?->visitor->person->name }}" disabled/>
+                                    <input type="text" class="form-control text-uppercase" name="visitor"
+                                        id="visitor" value="{{ $caution?->visitor->person->name }}" disabled />
                                 </div>
                                 <div class="form-group">
                                     <label for="sector_id">Destino</label>
                                     <select class="form-select form-control" name="sector_name" id="sector_id" disabled>
-                                        <option value="{{ $caution->visitor?->sector?->name }}" selected="selected">{{ $caution->visitor?->sector?->name }}</option>
+                                        <option value="{{ $caution->visitor?->sector?->name }}" selected="selected">
+                                            {{ $caution->visitor?->sector?->name }}</option>
                                     </select>
                                 </div>
                                 <div class="form-group">
                                     <label for="duty_user_id">Plantonista</label>
-                                    <select class="form-select form-control" name="duty_user_id" id="duty_user_id" disabled>
-                                        <option value="{{ $caution->dutyUser?->id }}" selected="selected">{{ $caution->dutyUser?->name }}</option>
+                                    <select class="form-select form-control" name="duty_user_id" id="duty_user_id"
+                                        disabled>
+                                        <option value="{{ $caution->dutyUser?->id }}" selected="selected">
+                                            {{ $caution->dutyUser?->name }}</option>
                                     </select>
                                 </div>
                                 <div class="form-group">
@@ -92,8 +120,10 @@
                                 </div>
 
                                 <div class="modal-footer">
-                                    <button type="submit" class="btn btn-success btn-sm text-white close-modal"><i class="fa fa-check"></i> Remover</button>
-                                    <button type="button" class="btn btn-danger btn-sm text-white close-btn" data-bs-dismiss="modal"><i class="fas fa-ban"></i> Cancelar</button>
+                                    <button type="submit" class="btn btn-success btn-sm text-white close-modal"><i
+                                            class="fa fa-check"></i> Remover</button>
+                                    <button type="button" class="btn btn-danger btn-sm text-white close-btn"
+                                        data-bs-dismiss="modal"><i class="fas fa-ban"></i> Cancelar</button>
                                 </div>
                             </form>
                         </div>
