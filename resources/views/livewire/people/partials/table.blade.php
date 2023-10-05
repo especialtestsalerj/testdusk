f6e1e66d842aa960a758856b9366624902ec801e
Breno Trengrouse <brenot@gmail.com>
2023-10-05 14:23:00 -0300

    card changes people + edit vg no photo

--- resources/views/livewire/people/partials/table.blade.php
+++ resources/views/livewire/people/partials/table.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div xmlns="http://www.w3.org/1999/html">
     <div class="row">
         @if ($showCard)
             @forelse ($people as $person)
@@ -49,56 +49,59 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="card-body">
+                        <div class="card-body py-0 ps-0">
                             <div class="row">
-                                <div class="col-3" data-label="Foto">
-                                    <img class="w-100" src="{{ $person->photo }}">
+                                <div class="col-5" data-label="Foto">
+                                    <img class="w-100 foto-card" src="{{ $person->photo }}">
                                 </div>
-                                <div class="col-9 d-flex align-items-start">
+
+
+                                <div class="col-7 pt-3">
                                     <div class="row">
                                         <div class="col-12">
                                             @if ($person->hasPendingVisitors())
                                                 <span class="badge bg-warning text-black mb-2">VISITA EM ABERTO</span>
                                             @endif
-
-                                            <div data-label="Documento">
-                                                @foreach ($person->documents as $document)
-                                                    <span>{{ $document->documentType->name }}</span>:
-                                                    {{ $document->numberMaskered }}<br>
-                                                @endforeach
-                                            </div>
                                         </div>
                                     </div>
-                                </div>
-{{--
-                                <div class="col-1 actions px-0 d-flex justify-content-center text-sm-center">
-                                    <div class="row">
-                                        @if ($person->hasPendingVisitors())
-                                            <div class="col-12">
-                                                <span class="btn btn-link px-3 py-1"
-                                                      wire:click="generateBadge({{ $person->pendingVisit->id }})"
-                                                      title="Imprimir Etiqueta">
-                                                    <i class="fa fa-lg fa-print"></i>
-                                                </span>
+                                    <div data-label="Documento" class="card-document-list">
+                                        @foreach ($person->documents as $document)
+                                            <div class="row bg-line-details mb-1 me-1">
+                                                <div class="col-10"><span>{{ $document->documentType->name }}</span>:<span class="fw-bold ms-2">{{ $document->numberMaskered }}</span></div>
+                                                <div class="col-2"><span class="btn btn-link px-0 py-0"><i class="fa fa-lg fa-check"></i></span></div>
                                             </div>
-                                        @endif
-                                        @can('people:show')
-                                            <div class="col-12">
-                                                <a href="{{ route('people.form', ['id' => $person->id, 'redirect' => $redirect, 'disabled' => true]) }}"
-                                                   class="btn btn-link px-0 pt-0 pb-1" title="Detalhar"><i
-                                                        class="fa fa-lg fa-search"></i></a>
-                                            </div>
-                                        @endCan
-                                        @can('people:update')
-                                            <div class="col-12">
-                                                <a href="{{ route('people.form', ['id' => $person->id, 'redirect' => $redirect, 'disabled' => false]) }}"
-                                                   class="btn btn-link px-0 pt-0 pb-1" title="Alterar"><i
-                                                        class="fa fa-lg fa-pencil"></i></a>
-                                            </div>
-                                        @endCan
+                                        @endforeach
                                     </div>
                                 </div>
-                                --}}
+                                {{--
+                                                                <div class="col-1 actions px-0 d-flex justify-content-center text-sm-center">
+                                                                    <div class="row">
+                                                                        @if ($person->hasPendingVisitors())
+                                                                            <div class="col-12">
+                                                                                <span class="btn btn-link px-3 py-1"
+                                                                                      wire:click="generateBadge({{ $person->pendingVisit->id }})"
+                                                                                      title="Imprimir Etiqueta">
+                                                                                    <i class="fa fa-lg fa-print"></i>
+                                                                                </span>
+                                                                            </div>
+                                                                        @endif
+                                                                        @can('people:show')
+                                                                            <div class="col-12">
+                                                                                <a href="{{ route('people.form', ['id' => $person->id, 'redirect' => $redirect, 'disabled' => true]) }}"
+                                                                                   class="btn btn-link px-0 pt-0 pb-1" title="Detalhar"><i
+                                                                                        class="fa fa-lg fa-search"></i></a>
+                                                                            </div>
+                                                                        @endCan
+                                                                        @can('people:update')
+                                                                            <div class="col-12">
+                                                                                <a href="{{ route('people.form', ['id' => $person->id, 'redirect' => $redirect, 'disabled' => false]) }}"
+                                                                                   class="btn btn-link px-0 pt-0 pb-1" title="Alterar"><i
+                                                                                        class="fa fa-lg fa-pencil"></i></a>
+                                                                            </div>
+                                                                        @endCan
+                                                                    </div>
+                                                                </div>
+                                                                --}}
 
                             </div>
 
@@ -160,15 +163,13 @@
                                                     <div class="col-5 col-lg-6 text-center text-lg-start">
                                                         <span class="fw-bold">{{ $person->name }}</span>
                                                         @if ($person->hasPendingVisitors())
-                                                            <span class="badge bg-warning text-black">VISITA EM
-                                                                ABERTO</span>
+                                                            <span class="badge bg-warning text-black">VISITA EM ABERTO</span>
                                                         @endif
                                                     </div>
                                                     <div class="col-3 col-lg-6 text-center text-lg-start">
                                                         @foreach ($person->documents as $document)
                                                             <span>{{ $document->documentType->name }}</span>:
                                                             {{ $document->numberMaskered }}
-                                                            &nbsp;
                                                         @endforeach
                                                     </div>
                                                 </div>
