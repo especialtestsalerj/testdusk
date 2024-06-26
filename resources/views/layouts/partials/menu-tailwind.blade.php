0f16a6f91742d125266e385cfdd2c765b6dede25
Breno Trengrouse <brenot@gmail.com>
2024-06-26 09:15:09 -0300

    calendar template tailwind 005

--- resources/views/layouts/partials/menu-tailwind.blade.php
+++ resources/views/layouts/partials/menu-tailwind.blade.php
@@ -1,6 +1,11 @@
 
 <header>
-    <nav class="border-gray-200 px-4 lg:px-6 py-2.5 bg-gray-800">
+    
+
+
+
+
+    <nav class="border-gray-200 px-4 lg:px-6 py-2.5 bg-brand-800">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="https://flowbite.com" class="flex items-center">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
@@ -16,7 +21,7 @@
                 </button>
             </div>
             <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                <ul class="flex flex-col mt-4 font-poppins font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     <li>
                         <a href="#" class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Home</a>
                     </li>
