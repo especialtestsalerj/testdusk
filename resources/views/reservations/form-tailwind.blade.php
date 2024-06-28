b88ce5d56fdb87e06a1fd5bceb9a46748352688e
marcoscoe <marcoscoe9@gmail.com>
2024-06-28 09:34:47 -0300

    booking

--- resources/views/reservations/form-tailwind.blade.php
+++ resources/views/reservations/form-tailwind.blade.php
@@ -1,7 +1,5 @@
-
-
-@extends('layouts.app-talwind')
+@extends('layouts.booking-talwind')
 
 @section('content')
-    <livewire:reservation.form-tailwind />
+    <livewire:reservation.form-tailwind/>
 @endsection
