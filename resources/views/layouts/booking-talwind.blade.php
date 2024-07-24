0e0e1a1c7f61f6818902fdf051af5c4c32796dd6
marcoscoe <marcoscoe9@gmail.com>
2024-07-24 14:11:30 -0300

    agendamento layout

--- resources/views/layouts/booking-talwind.blade.php
+++ resources/views/layouts/booking-talwind.blade.php
@@ -24,33 +24,29 @@
 
     <!-- Flatpickr CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" />
-{{--    <script src="https://cdn.tailwindcss.com"></script>--}}
-
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet"/>
+    {{--    <script src="https://cdn.tailwindcss.com"></script>--}}
     @include('layouts.partials.environment')
     @livewireStyles
 
 </head>
-<body class="bg-light">
+<body>
 <div id="app">
 
-
-    <main class="py-0" >
-        <div class="container-fluid">
-            <div class="row justify-content-center">
-                <div class="col">
+    <main class="py-4">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-center">
+                <div class="w-full">
                     @yield('content')
                     @if(isset($slot))
                         {{ $slot }}
-                    @endIf
+                    @endif
                 </div>
             </div>
         </div>
     </main>
 
-
-   {{-- @include('layouts.partials.footer')--}}
-
+    {{-- @include('layouts.partials.footer')--}}
 
 </div>
 
--- resources/views/livewire/agendamento/index.blade.php
+++ resources/views/livewire/agendamento/index.blade.php
@@ -1,16 +1,29 @@
-<div>
-    O que deseja Realizar?
-    <ul>
+<div class="bg-white p-6 rounded-lg shadow-md dark:bg-gray-800 dark:text-gray-100">
+    <h2 class="text-2xl font-bold text-gray-800 dark:text-gray-100 mb-4">O que deseja realizar?</h2>
+    <ul class="space-y-4 text-gray-700 dark:text-gray-300">
         <li>
-            <a href="{{route('agendamento.form-tailwind')}}" >Realizar um agendamento </a>
+            <a href="{{ route('agendamento.form-tailwind') }}"
+               class="flex items-center p-2 text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-600 rounded-lg transition-colors duration-200">
+                <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12l2 2 4-4m5 2a9 9 0 11-9-9 9 9 0 019 9z"></path>
+                </svg>
+                Realizar um agendamento
+            </a>
         </li>
         <li>
-            <a href="{{route('agendamento.form-group')}}" > Realizar um agendamento em Grupo </a>
+            <a href="{{ route('agendamento.form-group') }}"
+               class="flex items-center p-2 text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-600 rounded-lg transition-colors duration-200">
+                <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7M9 17l3 3m0 0l3-3m-3 3V10"></path>
+                </svg>
+                Realizar um agendamento em grupo
+            </a>
         </li>
-        <li>
+        <li class="flex items-center p-2 text-gray-800 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300 rounded-lg transition-colors duration-200">
+            <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-8v10a1 1 0 01-1 1H6a1 1 0 01-1-1V7a1 1 0 011-1h9l2-2h2a1 1 0 011 1v2z"></path>
+            </svg>
             Consultar Agendamentos
         </li>
     </ul>
-
-
 </div>
