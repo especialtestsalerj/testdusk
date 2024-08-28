d26b1087439c7948c96ed7a746e664686a42584a
marcoscoe <marcoscoe9@gmail.com>
2024-08-28 13:19:48 -0300

    emails

--- resources/views/emails/reservation_canceled.blade.php
+++ resources/views/emails/reservation_canceled.blade.php
@@ -1,41 +1,85 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reserva Cancelada</title>
+    <style>
+        body {
+            font-family: 'Helvetica Neue', Arial, sans-serif;
+            background-color: #e9ecef;
+            color: #333;
+            margin: 0;
+            padding: 0;
+            line-height: 1.6;
+        }
+        .container {
+            max-width: 600px;
+            margin: 50px auto;
+            background-color: #ffffff;
+            border-radius: 10px;
+            overflow: hidden;
+            box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            border: 1px solid #dee2e6;
+            position: relative;
+        }
+        .header {
+            background-color: #DC3545;
+            color: #ffffff;
+            padding: 15px;
+            text-align: center;
+            font-size: 24px;
+            font-weight: bold;
+            border-radius: 10px 10px 0 0;
+        }
+        .image-container {
+            text-align: center;
+            padding: 10px 0;
+        }
+        .image-container img {
+            max-width: 75%;
+            height: auto;
+            border-radius: 10px 10px 0 0;
+        }
+        .content {
+            padding: 20px;
+            text-align: center;
+        }
+        .content p {
+            font-size: 16px;
+            color: #333;
+        }
+        .footer {
+            font-size: 12px;
+            color: #6c757d;
+            text-align: center;
+            padding-top: 10px;
+            margin-top: 20px;
+            border-top: 1px solid #dee2e6;
+        }
+    </style>
 </head>
 <body>
-<h1>Olá {{$reservation->person['full_name']}}</h1>
-<p>Infelizmente sua reserva para o dia  {{ date_format($reservation->reservation_date,"d/m/Y") }} foi cancelada para maiores informações, entre em contato com o setor:
-    {{$reservation->sector?->nickname}}</p>
+<div class="container">
+    <!-- Espaço para imagem -->
+    <div class="image-container">
+        <img src="{{asset('img/logo-alerj-grande_azul.png')}}" alt="Reserva Cancelada">
+    </div>
+
+    <div class="header">
+        AGENDAMENTO CANCELADO
+    </div>
+
+    <div class="content">
+        <h1>Olá, {{$reservation->person['full_name']}}</h1>
+        <p style="font-size: 20px">Infelizmente, seu agendamento para o dia {{ date_format($reservation->reservation_date,"d/m/Y") }} foi cancelado.</p>
+        <p style="font-size: 20px">Para mais informações, por favor, entre em contato com o setor responsável: <strong>{{$reservation->sector?->nickname}}</strong>.</p>
+    </div>
+
+    <div class="footer">
+        <p>&copy; 2024 ALERJ - ASSEMBLEIA LEGISLATIVA DO ESTADO DO RIO DE JANEIRO</p>
+    </div>
+</div>
 </body>
 </html>
-
-{{--@extends('beautymail::templates.sunny')--}}
-
-{{--@section('content')--}}
-
-{{--    @include ('beautymail::templates.sunny.heading' , [--}}
-{{--        'heading' => 'Reserva Criada!',--}}
-{{--        'level' => 'h1',--}}
-{{--    ])--}}
-
-{{--    @include('beautymail::templates.sunny.contentStart')--}}
-
-{{--    <p>Sua reserva foi criada com os seguintes detalhes:</p>--}}
-
-{{--    <ul>--}}
-{{--        <li>ID da Reserva: 534543DDSQR</li>--}}
-{{--        <li>Data: 01/01/2024</li>--}}
-{{--        <li>Setor:  SETOR-TESTE</li>--}}
-{{--    </ul>--}}
-
-{{--    <p>Obrigado por usar nosso serviço!</p>--}}
-
-{{--    @include('beautymail::templates.sunny.contentEnd')--}}
-
-{{--    @include('beautymail::templates.sunny.button', [--}}
-{{--        	'title' => 'Para cancelar a visita clique aqui',--}}
-{{--        	'link' => 'http://google.com',--}}
-{{--    ])--}}
-
-{{--@stop--}}
